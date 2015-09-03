0e3a0aa624e3106634924c4fe303f81159542aaf
Andreas Stephan <astephan@localhost.localdomain>
2015-09-03 10:50:36 +0200

    route for database check

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'sql2.freemysqlhosting.net',
+    user: 'sql288857',
+    password: 'vB6*pR5!',
+    port: '3306',
+    database: 'sql288857'
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,12 +18,22 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (request, response) {
+    response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/check', function (req, res) {
+    connection.connect(function (err) {
+        if (!err) {
+            res.send("Database is connected ... \n\n");
+        } else {
+            res.send("Error connecting database ... \n\n");
+        }
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
