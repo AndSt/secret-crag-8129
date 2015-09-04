0e2dbc88c287de8d7c3dd5a93c76cbcc8ad13274
AndSt <andreas.stephan@tum.de>
2015-09-04 18:10:53 +0200

    test

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -12,7 +12,6 @@ function run() {
     var client = new Circuit.Client({domain: 'circuitsandbox.net'});
     client.logon('andreas-stephan@hotmail.de', 'andalos1').then(function (user) {
         console.log('Logged in as ' + user.displayName);
-        console.log(client.loggedOnUser);
     }).catch(function (err) {
         console.log('Unable to logon. ' + err);
     });
@@ -23,12 +22,12 @@ function run() {
         if (item.type === "TEXT"
                 && item.creatorId !== client.loggedOnUser.userId)
         {
-            var lol = item.text.split(':');
-            if (lol[0] === 'assistant') {
-                var object = JSON.parse(lol[1]);
-                console.log(object);
-                callback(false, object);
-            }
+//            var lol = item.text.split(':');
+//            if (lol[0] === 'assistant') {
+//                var object = JSON.parse(lol[1]);
+//                console.log(object);
+//                callback(false, object);
+//            }
 
 //            client.getUserById(item.creatorId, function (user) {
 //                var timeOffset = user.userPresenceState.timeZoneOffset;
