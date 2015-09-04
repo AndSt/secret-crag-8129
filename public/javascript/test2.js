bbd0bd7d3bdcf91b65cf84a65866ec9343f7874f
AndSt <andreas.stephan@tum.de>
2015-09-04 17:11:49 +0200

    test

--- public/javascript/test2.js
+++ public/javascript/test2.js
@@ -1,9 +1,8 @@
 
-
-var data = [
+var pieData = [
     {
         value: 300,
-        color:"#F7464A",
+        color: "#F7464A",
         highlight: "#FF5A5E",
         label: "Red"
     },
@@ -18,11 +17,35 @@ var data = [
         color: "#FDB45C",
         highlight: "#FFC870",
         label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
     }
 ];
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myPieChart = new Chart(ctx).Pie(data,{});
-
-
+var options = {
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\">" +
+            "<% for (var i=0; i<segments.length; i++){%>" +
+            "<li>" +
+            "<span style=\"background-color:<%=segments[i].fillColor%>\">" +
+            "</span>" +
+            "<%if(segments[i].label){%>" +
+            "<%=segments[i].label%><%}%>" +
+            "</li>" +
+            "<%}%>" +
+            "</ul>"
+};
+window.onload = function () {
+    var ctx = document.getElementById("chart-area").getContext("2d");
+    window.myPie = new Chart(ctx).Pie(pieData);
+};
 
