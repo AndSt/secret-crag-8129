d376c0beb63a379179c6906ce5f27736d05e1e95
AndSt <astephan@localhost.localdomain>
2015-09-04 10:29:18 +0200

    test

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -7,12 +7,24 @@ script.src = 'https://circuitsandbox.net/circuit.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function run() {
-    
+
     var client = new Circuit.Client({domain: 'circuitsandbox.net'});
 
     client.logon('andreas-stephan@hotmail.de', 'andalos1').then(function (user) {
         console.log('Logged in as ' + user.displayName);
-    }).catch(function (e) {
-        console.log('Unable to logon. ' + e);
+    }).catch(function (err) {
+        console.log('Unable to logon. ' + err);
     });
+
+    client.addEventListener('itemAdded').then(function (item) {
+        console.log("addedItem " + item.type);
+        client.logout().then(function () {
+            console.log("Ausgeloggt");
+        }).catch(function (err) {
+            consol.log("Ausloggen hat nicht funktioniert");
+        });
+    }).catch(function (err) {
+        console.log("Unable to get Item");
+    });
+
 }
