8846d4953e57db0039fa5ce1947ee495f761c1b7
AndSt <andreas.stephan@tum.de>
2015-09-04 16:31:24 +0200

    test

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -5,35 +5,37 @@ script.onload = function () {
 };
 script.src = 'https://circuitsandbox.net/circuit.js';
 document.getElementsByTagName('head')[0].appendChild(script);
-
 function run() {
 
     var client = new Circuit.Client({domain: 'circuitsandbox.net'});
-
     client.logon('andreas-stephan@hotmail.de', 'andalos1').then(function (user) {
         console.log('Logged in as ' + user.displayName);
-        client.addTextItem('0a19d4c4-9819-40c0-a299-ee3ce8ccb8b5', item2)
-                .then(function (i) {
-                    console.log(i);
-                });
     }).catch(function (err) {
         console.log('Unable to logon. ' + err);
     });
 
-    var item2 = {
-        contentType: "RICH",
-        content: "jo"
-    };
-
 
     client.addEventListener('itemAdded', function (event) {
         var item = event.item;
-        if (item.type === "TEXT") {
+        if (item.type === "TEXT"
+                && item.creatorId !== client.getLoggedOnUser().userId)
+        {
             console.log("In der Conversation " + item.convId +
                     " wurde gesagt: " + item.text.content);
+
+            client.addTextItem('0a19d4c4-9819-40c0-a299-ee3ce8ccb8b5',
+                    {
+                        contentType: "RICH",
+                        content: "jo"
+                    })
+                    .then(function (i) {
+                        console.log(i);
+                    });
+
         }
-
-
+    }).catch(function (err) {
+        console.log('Unable to logon. ' + err);
     });
-
 }
+
+
