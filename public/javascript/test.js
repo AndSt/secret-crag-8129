f763060b6fded0c8d38a02dd9cbef53150f6ca3c
AndSt <astephan@localhost.localdomain>
2015-09-04 10:22:34 +0200

    test

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -7,23 +7,12 @@ script.src = 'https://circuitsandbox.net/circuit.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function run() {
-    // Helper function to create a blob from a base64 string
-
-    console.log("hier");
-    // Create Circuit client instance
+    
     var client = new Circuit.Client({domain: 'circuitsandbox.net'});
-    console.log("hier2");
-    // Authenticate with Basic Auth
-    client.authenticate({email: 'andreas-stephan@hotmail.de',
-        password: 'andalos1'}).then(function () {
-        console.log("geschafft");
 
-        client.addEventListener('itemAdded', function (item) {
-            console.log(item.type);
-        });
-
-    }, function (err) {
-        console.log("verdammt");
-        console.error(err);
+    client.logon('andreas-stephan@hotmail.de', 'andalos1').then(function (user) {
+        console.log('Logged in as ' + user.displayName);
+    }).catch(function (e) {
+        console.log('Unable to logon. ' + e);
     });
 }
