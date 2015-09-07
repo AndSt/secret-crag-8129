565089a0c1c6dad1b833019f5e77b104e4b06377
AndSt <andreas.stephan@tum.de>
2015-09-07 13:49:35 +0200

    test

--- public/javascript/test2.js
+++ public/javascript/test2.js
@@ -47,6 +47,8 @@ var options = {
 };
 window.onload = function () {
     var ctx = document.getElementById("chart-area").getContext("2d");
-    window.myPie = new Chart(ctx).Pie(pieData, options);
+    var myPie = new Chart(ctx).Pie(pieData, options);
+    var legend = myPie.generateLegend();
+    document.getElementById('legend').value = legend;
 };
 
