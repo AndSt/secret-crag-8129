96b03d875f7ec77f8e6aebecca2fbf2c9eeff034
AndSt <andreas.stephan@tum.de>
2015-09-08 14:08:40 +0200

    test

--- assistant/textAnalyzer.js
+++ assistant/textAnalyzer.js
@@ -19,11 +19,12 @@ var getTextItemNumbers = function (convId, number) {
                                 var stats = [];
 
                                 participants.forEach(function (participant) {
-                                    stats[participant] = {
-                                        userId: participant,
-                                        numMessages: 0,
-                                        numLetters: 0
-                                    };
+                                    stats[participants.indexOf(participant)] =
+                                            {
+                                                userId: participant,
+                                                numMessages: 0,
+                                                numLetters: 0
+                                            };
                                 });
 
                                 var text;
@@ -37,7 +38,7 @@ var getTextItemNumbers = function (convId, number) {
 
                                 var logText = "";
                                 for (i = 0; i < participants.length; i++) {
-                                    logText = logText + JSON.stringify(stats[participants[i]]);
+                                    logText = logText + JSON.stringify(stats[i]);
                                 }
 
                                 logger.info("Statistiken für " + items.length +
@@ -56,75 +57,75 @@ var getTextItemNumbers = function (convId, number) {
 };
 
 var analyzeConversation = function (item, partials) {
-    
+
     logger.info("Textanalyzer: started analyzeConversation");
 
-return new Promise(function(resolve, reject){
-    var number = typeof partials[2] === 'undefined' ? 100 : partials[2];
+    return new Promise(function (resolve, reject) {
+        var number = typeof partials[2] === 'undefined' ? 100 : partials[2];
 
-    getTextItemNumbers(item.convId, number)
-            .then(function (data) {
+        getTextItemNumbers(item.convId, number)
+                .then(function (data) {
 
-                var outputData = [];
-                outputData[0] = {value: 300,
-                    color: "#F7464A",
-                    highlight: "#FF5A5E",
-                    label: "Red"
-                };
-                outputData[1] =
-                        {
-                            value: 0,
-                            color: "#46BFBD",
-                            highlight: "#5AD3D1",
-                            label: "Green"
-                        };
-                outputData[2] =
-                        {
-                            value: 0,
-                            color: "#FDB45C",
-                            highlight: "#FFC870",
-                            label: "Yellow"
-                        };
-                outputData[3] =
-                        {
-                            value: 0,
-                            color: "#949FB1",
-                            highlight: "#A8B3C5",
-                            label: "Grey"
-                        };
-                outputData[4] =
-                        {
-                            value: 0,
-                            color: "#4D5360",
-                            highlight: "#616774",
-                            label: "Dark Grey"
-                        };
+                    var outputData = [];
+                    outputData[0] = {value: 300,
+                        color: "#F7464A",
+                        highlight: "#FF5A5E",
+                        label: "Red"
+                    };
+                    outputData[1] =
+                            {
+                                value: 0,
+                                color: "#46BFBD",
+                                highlight: "#5AD3D1",
+                                label: "Green"
+                            };
+                    outputData[2] =
+                            {
+                                value: 0,
+                                color: "#FDB45C",
+                                highlight: "#FFC870",
+                                label: "Yellow"
+                            };
+                    outputData[3] =
+                            {
+                                value: 0,
+                                color: "#949FB1",
+                                highlight: "#A8B3C5",
+                                label: "Grey"
+                            };
+                    outputData[4] =
+                            {
+                                value: 0,
+                                color: "#4D5360",
+                                highlight: "#616774",
+                                label: "Dark Grey"
+                            };
 
-                data.sort(function (a, b) {
-                    if (a.numMessages < b.numMessages) {
-                        return -1;
+                    data.sort(function (a, b) {
+                        if (a.numMessages < b.numMessages) {
+                            return -1;
+                        }
+                        else if (a.numMessages > b.numMessages) {
+                            return 1;
+                        }
+                        else {
+                            return 0;
+                        }
+                    });
+
+                    var i = 0;
+                    while (i < data.length && i <= 4) {
+                        outputData[i].value = data[i].numMessages;
+                        outputData[i].label = data[i].userId;
+                        i++;
                     }
-                    else if (a.numMessages > b.numMessages) {
-                        return 1;
-                    }
-                    else {
-                        return 0;
-                    }
+                    logger.log(JSON.stringify(outputData));
+                    resolve("läuft gut");
+                })
+                .catch(function (err) {
+                    reject(err);
                 });
-
-                var i = 0;
-                while (i < data.length && i <= 4) {
-                    outputData[i].value = data[i].numMessages;
-                    outputData[i].label = data[i].userId;
-                    i++;
-                }
-                logger.log(JSON.stringify(outputData));
-                resolve("läuft gut");
-            })
-            .catch(function (err) {
-                reject(err);
-            });
-        });
+    });
 };
 
 
