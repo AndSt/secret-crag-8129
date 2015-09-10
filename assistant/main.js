98f29841b9193b4c6409293e9cd265d7eadd5cd3
AndSt <andreas.stephan@tum.de>
2015-09-10 17:46:33 +0200

    test

--- assistant/main.js
+++ assistant/main.js
@@ -18,8 +18,12 @@ var registerEventListener = function (client) {
         if (item.type === "TEXT"
                 && item.creatorId !== client.loggedOnUser.userId)
         {
-            sendToGA(item);
-            parseItem(item)
+            logger.info("its a text message");
+            sendToGA(item)
+                    .then(function (text) {
+                        logger.info(text);
+                    })
+                    .then(parseItem(item))
                     .then(function (text) {
                         comm.sendTextItem(item.convId, text);
                     })
@@ -59,68 +63,73 @@ var parseItem = function (item, callback) {
 };
 
 var sendToGA = function (item) {
+    return new Promise(function (resolve, reject) {
+        logger.info("sendToGA()");
+        var channel = {
+            id: item.convId
+        };
+        var user = {
+            id: item.creatorId
+        };
 
-    logger.info("sendToGA()");
-    var channel = {
-        id: item.convId
-    };
-    var user = {
-        id: item.creatorId
-    };
+        var msgText = item.text.content;
 
-    var msgText = item.text.content;
-
-    //2 algorithms to count different things in the message text
-    function searchM(regex) {
-        var searchStr = msgText.match(regex);
-        if (searchStr !== null) {
-            return searchStr.length;
+        //2 algorithms to count different things in the message text
+        function searchM(regex) {
+            var searchStr = msgText.match(regex);
+            if (searchStr !== null) {
+                return searchStr.length;
+            }
+            return 0;
         }
-        return 0;
-    }
-    ;
+        ;
 
-    function searchS(regex) {
-        var searchStr = msgText.split(regex);
-        if (searchStr !== undefined) {
-            return searchStr.length;
+        function searchS(regex) {
+            var searchStr = msgText.split(regex);
+            if (searchStr !== undefined) {
+                return searchStr.length;
+            }
+            return 0;
         }
-        return 0;
-    }
-    ;
+        ;
 
-    var wordCount = searchS(/\s+\b/);
-    var exclaCount = searchM(/!/g);
-    var questionCount = searchM(/\?/g);
-    var letterCount = msgText.length;
-    //The Structure Data! This is where are the pretty GA data gets gathered
-    //before it is sent to the GA servers for us to analyse at a later time.
-    var data = {
-        v: 1,
-        tid: "UA-41507980-2", // <-- ADD UA NUMBER
-        cid: user.id,
-        ds: "circuit", //data source
-        cs: "circuit", // campaign source
-        cd1: user.id,
-        cd2: channel.id,
-        cd3: msgText,
-        cm1: wordCount,
-        cm2: letterCount,
-        cm3: exclaCount,
-        cm4: questionCount, //need to set up in GA
-        t: "event",
-        ec: "slack: " + channel.id,
-        ea: "post by " + user.id,
-        el: msgText,
-        ev: 1
-    };
-    logger.info("https://www.google-analytics.com/collect?" + qs.stringify(data));
-    request.post("https://www.google-analytics.com/collect?" + qs.stringify(data),
-            function (error, resp, body) {
-                logger.error(JSON.stringify(error));
-                logger.info(JSON.stringify(resp));
-                logger.ino(JSON.stringify(body));
-            });
+        var wordCount = searchS(/\s+\b/);
+        var exclaCount = searchM(/!/g);
+        var questionCount = searchM(/\?/g);
+        var letterCount = msgText.length;
+        //The Structure Data! This is where are the pretty GA data gets gathered
+        //before it is sent to the GA servers for us to analyse at a later time.
+        var data = {
+            v: 1,
+            tid: "UA-41507980-2", // <-- ADD UA NUMBER
+            cid: user.id,
+            ds: "circuit", //data source
+            cs: "circuit", // campaign source
+            cd1: user.id,
+            cd2: channel.id,
+            cd3: msgText,
+            cm1: wordCount,
+            cm2: letterCount,
+            cm3: exclaCount,
+            cm4: questionCount, //need to set up in GA
+            t: "event",
+            ec: "slack: " + channel.id,
+            ea: "post by " + user.id,
+            el: msgText,
+            ev: 1
+        };
+        logger.info("https://www.google-analytics.com/collect?" + qs.stringify(data));
+        request.post("https://www.google-analytics.com/collect?" + qs.stringify(data),
+                function (error, resp, body) {
+                    logger.error(JSON.stringify(error));
+                    logger.info(JSON.stringify(resp));
+                    logger.ino(JSON.stringify(body));
+                    if (!error) {
+                        resolve("passt");
+                    }
+                });
+        resolve("shit");
+    });
 };
 
 var update = function () {
