a28974d3b94cce0ea5486592ef1e2e98da6b8055
AndSt <andreas.stephan@tum.de>
2015-09-11 15:49:15 +0200

    test

--- public/javascript/statistics.js
+++ public/javascript/statistics.js
@@ -1,54 +1,53 @@
-var pieData = [
-    {
-        value: 300,
-        color: "#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    },
-    {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-    },
-    {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
-    }
-];
-
-
-var options = {
-    legendTemplate:
-            "<ul class=\"<%=name.toLowerCase()%>-legend\">" +
-            "<% for (var i=0; i<segments.length; i++){%>" +
-            "<li>" +
-            "<span style=\"background-color:<%=segments[i].fillColor%>\">" +
-            "</span>" +
-            "<%if(segments[i].label){%>" +
-            "<%=segments[i].label%><%}%>" +
-            "</li>" +
-            "<%}%>" +
-            "</ul>"
-};
-
-var urlOptions = window.location.pathname.split('/');
 
 var initialize = function (numMessagesData) {
+
+    var pieData = [
+        {
+            value: 300,
+            color: "#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
+    ];
+
+
+    var options = {
+        legendTemplate:
+                "<ul class=\"<%=name.toLowerCase()%>-legend\">" +
+                "<% for (var i=0; i<segments.length; i++){%>" +
+                "<li>" +
+                "<span style=\"background-color:<%=segments[i].fillColor%>\">" +
+                "</span>" +
+                "<%if(segments[i].label){%>" +
+                "<%=segments[i].label%><%}%>" +
+                "</li>" +
+                "<%}%>" +
+                "</ul>"
+    };
 
     var data1 = pieData.slice();
     var i = 0;
@@ -84,13 +83,14 @@ var initialize = function (numMessagesData) {
 
 
 $(document).ready(function () {
+    var urlOptions = window.location.pathname.split('/');
+    console.log("urlOptions: " + urlOptions);
     $.get("/getStats/" + urlOptions[2] + "/numMessages", function (data) {
+        console.log("data: " + data);
         initialize(data);
-
     });
 });
 
-console.log("urlOptions: " + urlOptions);
 
 
 
