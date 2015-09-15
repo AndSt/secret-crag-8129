06e3bc5957022e86dd97b90958e237caad5e7d94
AndSt <andreas.stephan@tum.de>
2015-09-15 14:14:24 +0200

    off

--- utils/client.js
+++ utils/client.js
@@ -7,13 +7,22 @@ var logger = require('./logger');
 
 
 var client = new Circuit.Client({domain: 'circuitsandbox.net'});
-client.logon('andreas-stephan@hotmail.de', 'andalos1')
-        .then(function (user) {
-            logger.info('Logged in as ' + user.displayName);
+console.log("test");ölkjölj
+client.authenticate({email: 'andreas-stephan@hotmail.de', password: 'andalos1'})
+        .then(function () {
+            console.log("logged in ");
         })
-        .catch(function (err) {
-            logger.error('Unable to logon. ' + err);
+        .catch(function () {
+            console.log("not logged in");
         });
+//client.logon('andreas-stephan@hotmail.de', 'andalos1')
+//        .then(function (user) {
+//            console.log("eingeloggt");
+////            logger.info('Logged in as ' + user.displayName);
+//        })
+//        .catch(function (err) {
+//            logger.error('Unable to logon. ' + err);
+//        });
 
 var getClient = function () {
     return client;
